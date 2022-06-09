15bb5f65fc847f45c425f103609353b8db64bed7
Theophilus Boakye <93124888+TheophilusBoakye@users.noreply.github.com>
2022-06-09 10:20:56 +0000

    added security packages

--- app.js
+++ app.js
@@ -2,21 +2,36 @@ require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
 const app = express();
+
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
 // const logger = require("morgan");
 // local imporrs
 const authRouter = require("./routes/auth");
 const adminsRouter = require("./routes/admins");
 const userRouter = require("./routes/user");
 const connectDB = require("./db/connect");
+
 // middleware imports
 const notFoundMiddleware = require("./middleware/custom-error");
 const errorMiddleware = require("./middleware/custom-error");
 const authMiddleware = require("./middleware/authentication");
 const librarianAuthMiddleware = require("./middleware/adminAuthMiddleware");
+
+// extra security packages
+app.use(
+	rateLimiter({
+		windowMs: 15 * 60 * 1000, // 15 minutes
+		max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	})
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 app.use(express.json());
 
-// routes
-// app.use(logger("dev"));
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/admin", authMiddleware, librarianAuthMiddleware, adminsRouter);
 app.use("/api/v1", authMiddleware, userRouter);
